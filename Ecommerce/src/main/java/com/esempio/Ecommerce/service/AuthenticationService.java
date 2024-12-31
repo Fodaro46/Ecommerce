c43f2e9f528f745ff36b44c4b29e2f861a8fd9d0
Fodaro46 <fdrlnz01h04c352x@studenti.unical.it>
2024-12-31 18:22:40 +0100

    Update

---
+++ Ecommerce/src/main/java/com/esempio/Ecommerce/service/AuthenticationService.java
@@ -0,0 +1,14 @@
+package com.esempio.Ecommerce.service;
+
+import com.esempio.Ecommerce.api.dto.RegistrationBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService extends EncryptionService {
+    public void register(RegistrationBody request) {
+
+    }
+
+}
